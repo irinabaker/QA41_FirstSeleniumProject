e37266e886f7098b76c4bd4b5aaa68fa35e3792e
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2024-06-10 21:04:45 +0200

    First commit

---
+++ src/test/java/com/ait/tests/FirstSeleniumTest.java
@@ -0,0 +1,39 @@
+package com.ait.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+
+    WebDriver driver;
+
+    //before - setUp
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        //driver.get("https://www.google.com");
+
+        driver.navigate().to("https://www.google.com");//with history
+
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+    }
+
+    //tests
+    @Test
+    public void openGoogle() {
+        System.out.println("Google opened!");
+    }
+
+    //after - tearDown
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        //interview question
+        driver.quit();//all tabs && close browser
+    //    driver.close();//only one tab(if only one tab -> close browser)
+    }
+}
